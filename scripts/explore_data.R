3d48182b31432fa4667fedec80c1251f21850348
Marcos Galan Ganga <marcosgalan@ub.edu>
2024-01-26 13:48:43 +0100

    explore_data with PCA, clustering and heatmap

--- scripts/explore_data.R
+++ scripts/explore_data.R
@@ -70,3 +70,200 @@ ggplot(data = top_loadings) +
   geom_text(aes(x = PC1, y = PC2, label = gene),
             nudge_y = 0.005, size = 3) +
   scale_x_continuous(expand = c(0.02, 0.02))
+
+
+#Friday 26th January
+loadings_plot <- ggplot(data = top_loadings) +
+  geom_segment(aes(x = 0, y = 0, xend = PC1, yend = PC2),
+               arrow = arrow(length = unit(0.1, "in")),
+               color = "red") +
+  geom_text(aes(x = PC1, y = PC2, label = gene),
+            nudge_y = 0.005, size = 3) +
+  scale_x_continuous(expand = c(0.02, 0.02))
+
+pca_plot <- pc_scores %>% 
+  full_join(sample_info, by = "sample") %>% 
+  ggplot(aes(x = PC1, y = PC2,
+             color = factor(minute),
+             shape = strain)) +
+  geom_point()
+
+library(patchwork)
+
+(pca_plot | loadings_plot)
+
+(pca_plot / loadings_plot)
+
+(pca_plot | pca_plot | pca_plot) / loadings_plot
+
+(pca_plot | pca_plot | pca_plot) / loadings_plot +
+  plot_annotation(tag_levels = "A")
+
+library(ggfortify)
+autoplot(sample_pca)
+
+autoplot(sample_pca, data = sample_info, colour = "minute", shape = "strain")
+
+library(broom)
+
+tidy(sample_pca, matrix = "eigenvalues")
+tidy(sample_pca, matrix = "loadings")
+
+
+#Francesco Tabares
+
+autoplot(sample_pca,
+         data = sample_info %>% mutate(minute = as.factor(minute)),
+         colour = "minute",
+         shape = "strain")
+
+# Differential expression results
+test_results <- read_csv("data_rnaseq/test_result.csv")
+
+# Columns:
+# Gene -> With the gene name.
+# baseMean -> The normalized expression level of a gene
+# log2FoldChange -> The amount of change between 2 conditions
+# lfcSE -> Standard error associated to log2FoldChange value
+# stat -> Statistics value computed as log2FoldChange / lfcSE and then compared to standard normal distribution
+# pvalue -> p-value associated with the change
+# padj-> p-value corrected for multiple hypothesis testing
+# comparison -> comparison group
+
+
+# MA plot
+# We would generate a MA plot (baseMean vs log2FoldChange) and organize panels by comparison (timepoint)
+
+test_results %>% 
+  ggplot(aes(x = log(baseMean), y = log2FoldChange)) +
+  geom_point(alpha = 0.1) +
+  facet_wrap(facets = vars(comparison))
+
+test_results %>% 
+  mutate(sig = ifelse(padj < 0.01, log2FoldChange, NA)) %>% 
+  ggplot(aes(x = log10(baseMean), y = log2FoldChange)) +
+  geom_point(alpha = 0.1) +
+  geom_point(aes(y=sig), color = "tomato", size = 1) +
+  facet_wrap(facets = vars(comparison))
+
+test_results %>% 
+  mutate(sig = ifelse(padj < 0.01, log2FoldChange, NA)) %>% 
+  ggplot(aes(x = log10(baseMean), y = log2FoldChange)) +
+  geom_point(alpha = 0.1) +
+  geom_point(aes(y=sig), color = "tomato", size = 1) +
+  geom_hline(yintercept = 0, color = "dodgerblue") +
+  facet_wrap(facets = vars(comparison))
+
+ma_plot <- test_results %>% 
+  mutate(sig = ifelse(padj < 0.01, log2FoldChange, NA)) %>% 
+  ggplot(aes(x = log10(baseMean), y = log2FoldChange)) +
+  geom_point(alpha = 0.1) +
+  geom_point(aes(y=sig), color = "tomato", size = 1) +
+  geom_hline(yintercept = 0, color = "dodgerblue") +
+  facet_wrap(facets = vars(comparison))
+
+pca_plot <- autoplot(sample_pca,
+                     data = sample_info %>% mutate(minute = as.factor(minute)),
+                     colour = "minute",
+                     shape = "strain")
+
+(ma_plot | pca_plot)
+
+#Visualizing expression trends
+# 1. Get candidate gene (aka padj < 0.01)
+candidate_gene <- test_results %>% 
+  filter(padj < 0.01) %>% 
+  pull(gene) %>% # same as writing test_results[,"gene"] or test_results$gene
+  unique()
+
+# 1a. Get the trans_cts in long format
+trans_cts <- read_csv("data_rnaseq/counts_transformed.csv")
+trans_cts_long <- trans_cts %>% 
+  pivot_longer(cols = wt_0_r1:mut_180_r3, names_to = "sample", values_to = "cts") %>% 
+  full_join(sample_info, by = "sample")
+
+# 2. Filter trans_cts_long for candidate genes and compute mean expression value for each gene in each timepoint and each genotype
+trans_cts_mean <- trans_cts_long %>% 
+  filter(gene %in% candidate_gene) %>% 
+  group_by(gene, strain, minute) %>% 
+  summarize(mean_cts = mean(cts), nrep = n()) %>% 
+  ungroup()
+
+# 3. Plot trends
+trans_cts_mean %>% 
+  ggplot(aes(x = minute, y = mean_cts)) +
+  geom_line(aes(group = gene), alpha = 0.3) +
+  facet_grid(rows = vars(strain))
+
+# Scaling data to improve visualization
+trans_cts_mean <- trans_cts_long %>% 
+  filter(gene %in% candidate_gene) %>% 
+  group_by(gene) %>% 
+  mutate(cts_scaled = (cts - mean(cts)) / sd(cts)) %>% 
+  group_by(gene, strain, minute) %>% 
+  summarize(mean_cts_scaled = mean(cts_scaled),
+            nrep = n()) %>% 
+  ungroup()
+  
+trans_cts_mean %>% 
+  ggplot(aes(x = minute, y = mean_cts_scaled)) +
+  geom_line(aes(group = gene), alpha = 0.3) +
+  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
+  facet_grid(rows = vars(strain)) +
+  scale_x_continuous(breaks = unique(trans_cts_mean$minute))
+
+#Clustering
+trans_cts <- read_csv("data_rnaseq/counts_transformed.csv")
+
+# 1. Create a matrix of counts
+hclust_matrix <- trans_cts %>% 
+  select(-gene) %>% 
+  as.matrix()
+
+#assign rownames
+rownames(hclust_matrix) <- trans_cts$gene
+hclust_matrix <- hclust_matrix[candidate_gene,]
+
+hclust_matrix <- hclust_matrix %>% 
+  #transpose the matrix so genes are as columns
+  t() %>% 
+  #apply scaling to each column of the matrix (genes)
+  scale() %>% 
+  #transpose back so genes are as rows again
+  t()
+
+
+gene_dist <- dist(hclust_matrix)
+
+# Hierarchical clustering
+gene_hclust <- hclust(gene_dist, method = "complete")
+
+plot(gene_hclust, labels = F)
+
+plot(gene_hclust, labels = F) +
+  abline(h = 10, col="red", lwd = 2)
+
+
+# Make clusters based on the number that I want
+cutree(gene_hclust, k = 5)
+
+gene_cluster <- cutree(gene_hclust, k = 5) %>% 
+  enframe()
+
+gene_cluster <- cutree(gene_hclust, k = 5) %>% 
+  enframe() %>% 
+  rename(gene = name, cluster = value)
+
+trans_cts_cluster <- trans_cts_mean %>% 
+  inner_join(gene_cluster, by  ="gene")
+
+trans_cts_cluster %>% 
+  ggplot(aes(x = minute, y = mean_cts_scaled)) +
+  geom_line(aes(group = gene)) +
+  facet_grid(cols = vars(cluster), rows = vars(strain))
+
+# Heatmap
+#BiocManager::install("ComplexHeatmap")
+library(ComplexHeatmap)
+
+Heatmap(hclust_matrix, show_row_names = F)
